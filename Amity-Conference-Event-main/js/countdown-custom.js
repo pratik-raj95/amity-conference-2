4b233394e80472f655c21038a56f6b1847361185
Pratik raj <pratikraj9117@gmail.com>
2024-06-14 10:52:38 +0530

    slider

---
+++ Amity-Conference-Event-main/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date("sept 26, 2024 15:37:25")}); // year, month, date, hour
+        });
+});		
+
